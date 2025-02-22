746d345be71599f0225527668105cba6e1ac7f90
Jewel <nanaafianyamekye2@gmail.com>
2025-02-22 00:49:05 +0000

    Installed stripe for payment

--- cakeshop/settings.py
+++ cakeshop/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+STRIPE_PUBLIC_KEY = 'your-stripe-public-key'
+STRIPE_SECRET_KEY = 'your-stripe-secret-key'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'shop',
     'cart',
+    'stripe',
 ]
 CART_SESSION_ID = 'cart'
 
