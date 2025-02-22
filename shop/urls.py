bd7122c3f6d0ca90e6ca71f1a2acaba7600a1c2f
Jewel <nanaafianyamekye2@gmail.com>
2025-02-22 02:00:37 +0000

    modified templates/shop/cart.html

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('category/<int:category_id>/', views.category_detail, name='category_detail'),
     path('cake/<int:cake_id>/', views.cake_detail, name='cake_detail'),
+    path('create-checkout-session/', views.create_checkout_session, name='create _checkout_session'),
+    path('success/', views.success, name='success'),
+    path('cancel/', views.cancel, name='cancel'),
     ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,37 @@
+import stripe
 from django.shortcuts import render
 from .models import Cake, Category
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+def create_checkout_session_session(request):
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency':
+'usd',
+'product_data': {
+    'Cake Order',
+                           },
+                           'unit_amount':
+2000, # $20.00
+                        },
+                        'quantity': 1,
+                        },
+                        ],
+                        mode='payment',
+success_url=request.build_absolute_uri('/shop/success/'),
+cancel_url=request.build_absolute_uri('/shop/cancel/'),
+    )
+        return JsonResponse({'id': checkout_session.id})
+    except Exception as e:
+        return JsonResponse({'error': str(e)})
 
 def index(request):
     categories = Category.objects.all()
