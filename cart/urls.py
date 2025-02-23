d1617641d9b3f3ffb7edebbe6adb71db485d33d4
Jewel <nanaafianyamekye2@gmail.com>
2025-02-23 02:04:03 +0000

    modified:   cart/urls.py        modified:   cart/views.py        modified:   shop/templates/shop/navbar.html, and created cart/templates/

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'cart'
+
 urlpatterns = [
     path('', views.cart_home, name='cart_home'),  # Example view
+    path('', views.cart_detail, name='cart_detail'),
+    path('add/<int:cake_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from shop.models import Cake
+from .models import Cart, CartItem
 
 def cart_home(request):
     return HttpResponse("This is the Cart page.")
+
+def cart_detail(request):
+    cart, created = Cart.objects.get_or_create(session_key=request.session.session_key)
+    return render(request, 'cart/cart_detail.html', {'cart': cart})
+
+def add_to_cart(request, cake_id):
+    cake = get_object_or_404(Cake, id=cake_id)
+    cart, created = Cart.objects.get_or_create(session_key=request.session.session_key)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, cake=cake)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+    return redirect('cart:cart_detail')
+
+def remove_from_cart(request, cart_item_id):
+    cart_item = get_object_or_404(CartItem, id=cart_item_id)
+    cart_item.delete()
+    return redirect('cart:cart_detail')
+
+
 
