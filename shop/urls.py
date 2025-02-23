ee3609e0ab619da10864b62b2dcc5ca14603255c
Jewel <nanaafianyamekye2@gmail.com>
2025-02-23 14:56:03 +0000

    Modified shop/__pycache__/urls.cpython-312.pyc, and shop/urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'shop'
+
 urlpatterns = [
     path('', views.index, name='index'), # Home page
     path('cart/', views.cart_detail, name='cart_detail'), # Cart page
