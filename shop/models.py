7e015b22d2e252601de38e7550f5ab2bf1981835
Jewel <nanaafianyamekye2@gmail.com>
2025-03-02 15:51:16 +0000

    modified:   shop/__pycache__/models.cpython-312.pyc        modified:   shop/models.py        modified:   shop/templates/shop/home.html

--- shop/models.py
+++ shop/models.py
@@ -12,8 +12,7 @@ class Cake(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='cakes/')  # Ensure you have this field
+    image = models.ImageField(upload_to='cakes/', null=True, blank=True)  # Allow null and blank values
 
     def __str__(self):
         return self.name
